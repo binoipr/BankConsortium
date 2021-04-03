d036afa3226750a7bf4e3a4859878d4450bd604c
Binoy <binoi.pr@gmail.com>
2021-04-03 11:46:38 +0530

    mongodb connection established

--- models/VehicleRegister.js
+++ models/VehicleRegister.js
@@ -5,8 +5,7 @@ const Schema = mongoose.Schema;
 const vehicleRegisterSchema = new Schema({
   vehicle: { type: String, default: "bike", enum: ["bike", "car"] },
   company_registerNo: { type: Number, default: 0, unique: true },
-  company_name: { type: String, required: true, trim: true },
-  vehicle_name: { type: String, required: true },
+  company_name: { type: String, required: true },
 });
 
 module.exports = mongoose.model("Vehicle", vehicleRegisterSchema);
--- routes/registerRoute.js
+++ routes/registerRoute.js
@@ -3,15 +3,43 @@ const router = express.Router();
 
 const Bank = require("../models/BankRegister");
 const Vehicle = require("../models/VehicleRegister");
-
+const VehicleList = require("../models/VehicleList");
 //@route  POST /register/bank
 //@desc   Register a bank
 //@access public
-router.post("/bank", (req, res) => {});
+router.post("/bank", (req, res) => {
+  const { name, email, password } = req.body;
+  const newBank = new Bank({
+    name,
+    email,
+    password,
+  });
+  newBank.save().then((bank) => res.json(bank));
+});
 
 //@route  POST /register/vehicle
 //@desc   Register a vehicle company
 //@access public
-router.post("/vehicle", (req, res) => {});
+router.post("/vehicle", (req, res) => {
+  const { vehicle, company_registerNo, company_name } = req.body;
+  const newCompany = new Vehicle({
+    vehicle,
+    company_registerNo,
+    company_name,
+  });
+  newCompany.save().then((company) => res.json(company));
+});
+
+//@route  POST /register/vehicle/add
+//@desc   Adding vehicle names
+//@access public
+router.post("/vehicle/add", (req, res) => {
+  const { company_registerNo, vehicle_name } = req.body;
+  const newVehicle = new VehicleList({
+    company_registerNo,
+    vehicle_name,
+  });
+  newVehicle.save().then((veh_item) => res.json(veh_item));
+});
 
 module.exports = router;
