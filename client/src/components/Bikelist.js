2d92bcb38993fdb1352f21ddae85906f638434ee
Binoy <binoi.pr@gmail.com>
2021-04-13 23:54:41 +0530

    f List created

---
+++ client/src/components/Bikelist.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import { Button } from "reactstrap";
+
+class BikeList extends Component {
+  state = {
+    Vehicle: [
+      {
+        name: "",
+        member_No: "",
+        mobile_No: "",
+        vehicle_bookingNo: "",
+        downpaymen: "",
+        loan_amount: "",
+        total_amount: "",
+        interest: "",
+        booking_date: "",
+        expected_delivery: "",
+      },
+    ],
+  };
+
+  onDeleteClick = (id) => {};
+  render() {
+    return (
+      <div>
+        <div className="ListHeading">
+          <div className="primary-title">
+            <p>Bike List</p>
+          </div>
+        </div>
+
+        <div className="latest-products">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12">
+                <div className="section-heading">
+                  <h2>Latest Products</h2>
+                  {/* <a href="products.html">
+                    view all products <i className="fa fa-angle-right"></i>
+                  </a> */}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default BikeList;
